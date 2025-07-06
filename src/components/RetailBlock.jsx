6f70f7e9ff83de732f7a7318085263be01e15136
GLokeshchary <lokeshchary648@gmail.com>
2025-07-06 21:10:02 +0530

    second commit

--- src/components/RetailBlock.jsx
+++ src/components/RetailBlock.jsx
@@ -8,7 +8,7 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
 // import required modules
-import { EffectCoverflow, Pagination } from "swiper/modules";
+import { Autoplay, EffectCoverflow, Pagination } from "swiper/modules";
 import "./RetailBlock.css";
 const RetailBlock = ({ list }) => {
   return (
